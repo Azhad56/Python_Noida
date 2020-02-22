aa0d137e0ecda9ca5bf6fcabddd247ee1608702c
Azhad Ghufran <Azhad56@gmail.com>
2020-02-23 01:08:00 +0530

    git and some lectures

--- lecture18/nqueens.py
+++ lecture18/nqueens.py
@@ -1,18 +1,37 @@
-def queens(board,row,n):
+def queens(board, row, n):
+
     if row == n:
-        print("Solution mil gya")
+        print("solution mil gya")
         return
-    for col in range(0,n):
-        if is_safe(board,row,col,n):
+
+    for col in range(0, n):
+        if is_safe(board, row, col, n):
             board[row][col] = True
-            queens(board,row+1,n)
+            queens(board, row+1, n)
             board[row][col] = False
-def is_safe(board,row,col,n):
-    for i in range(0,col):
-        if board[row][i]:
-            return False
-    steps = min(row,col)
-    for step in steps:
-        if board[row-step][col-step]:
-            return False
-    steps = min(row,)
+
+
+def is_safe(board, row, col, n):
+
+    max_steps = max(row, col)
+
+    for step in range(1, max_steps + 1):
+
+        # if we can go up
+        if row - step >= 0:
+            if board[row - step][col]:
+                return False
+
+            # let's check left diag
+            if col - step >= 0:
+                if board[row-step][col-step]:
+                    return False
+
+            # let's check right diag
+            if col + step < n:
+                if board[row - step][col + step]:
+                    return False
+
+    return True
+board = [[False]*4 for i in range(4)]
+print(queens(board,row=0,n=4))
---
+++ lecture18/practice.py
@@ -0,0 +1,1 @@
+def queens(board,row,n)
